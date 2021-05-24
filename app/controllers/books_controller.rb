09df2a8726acdb375009b9a9223fbf4a94644662
kitaura19960703 <kitaura19960703@icloud.com>
2021-05-24 09:12:05 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+  end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if@book.save
+      redirect_to book_path(@book)
+      flash[:notice] = 'successfully'
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id)
+    flash[:notice] = 'successfully'
+    else
+    render action: :edit
+    end
+  end
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user_id == current_user.id
+    render "edit"
+    else
+    redirect_to books_path
+    end
+  end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '43b8e29a48377b2e9c665a6709e4837f0c660f572b88223e2c93f13f1192f960cd98dead05355fb900cf4b0acfcd8b075dcac4c05742ca21c2ef0c4f7155000b'
